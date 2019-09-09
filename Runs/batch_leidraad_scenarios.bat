cc1586cfc7990216e7d68329f76bff37a9be67d1
basvanbemmel <basvanbemmel@b1a6ab50-0fb6-e611-ad82-1cc1de80c1ee>
2019-09-09 09:55:23 +0000

    Folckert: toevoeging batchfile om scenarios te combineren
    git-svn-id: https://pbl.sliksvn.com/vesta/PD/branch/Vesta35beta@234 b1a6ab50-0fb6-e611-ad82-1cc1de80c1ee

---
+++ Runs/batch_leidraad_scenarios.bat
@@ -0,0 +1,46 @@
+REM GeoDmsRun.exe [/PProjName] [/LLogFileName] ConfigFileName ItemNames
+REM [] betekent optioneel
+REM Sluit GUI-variant(GeoDmsGui.exe) af bij aanroepen van GeoDmsRun.exe (indien dit dezelfde is)
+REM Resultaten worden weggeschreven in: n:/LD/%confignaam%/results/...
+REM Bij nogmaals runnen worden bestanden gewoon overschreven
+
+set pd_root=N:\PD\Runs_Leidraad\Default_Configuratie
+set exe=GeoDms7182
+
+set CUR_YYYY=%date:~10,4%
+set CUR_MM=%date:~4,2%
+set CUR_DD=%date:~7,2%
+set CUR_HH=%time:~0,2%
+if %CUR_HH% lss 10 (set CUR_HH=0%time:~1,1%)
+set CUR_NN=%time:~3,2%
+set CUR_SS=%time:~6,2%
+set CUR_MS=%time:~9,2%
+set SUBFILENAME=%CUR_YYYY%%CUR_MM%%CUR_DD%-%CUR_HH%%CUR_NN%%CUR_SS%
+
+if not exist %pd_root% goto error
+
+"N:\prog\ObjectVision\%exe%\GeoDmsRun.exe" /L"N:\log\%SUBFILENAME%_Referentie_StartJaar_S1_Hoofdindicatoren.txt" "%pd_root%\Runs\Referentie.dms" /Resultaten/StartJaar/PlanRegioResults/Vergelijking/meerkosten/Scenario_1/Hoofdindicatoren
+"N:\prog\ObjectVision\%exe%\GeoDmsRun.exe" /L"N:\log\%SUBFILENAME%_Referentie_StartJaar_S1_NationaleKosten.txt" "%pd_root%\Runs\Referentie.dms" /Resultaten/StartJaar/PlanRegioResults/Vergelijking/meerkosten/Scenario_1/NationaleKosten
+"N:\prog\ObjectVision\%exe%\GeoDmsRun.exe" /L"N:\log\%SUBFILENAME%_Referentie_StartJaar_S1_Aansluitingen.txt" "%pd_root%\Runs\Referentie.dms" /Resultaten/StartJaar/PlanRegioResults/Vergelijking/meerkosten/Scenario_1/Aansluitingen
+
+"N:\prog\ObjectVision\%exe%\GeoDmsRun.exe" /L"N:\log\%SUBFILENAME%_Referentie_StartJaar_S2_Hoofdindicatoren.txt" "%pd_root%\Runs\Referentie.dms" /Resultaten/StartJaar/PlanRegioResults/Vergelijking/meerkosten/Scenario_2/Hoofdindicatoren
+"N:\prog\ObjectVision\%exe%\GeoDmsRun.exe" /L"N:\log\%SUBFILENAME%_Referentie_StartJaar_S2_NationaleKosten.txt" "%pd_root%\Runs\Referentie.dms" /Resultaten/StartJaar/PlanRegioResults/Vergelijking/meerkosten/Scenario_2/NationaleKosten
+"N:\prog\ObjectVision\%exe%\GeoDmsRun.exe" /L"N:\log\%SUBFILENAME%_Referentie_StartJaar_S2_Aansluitingen.txt" "%pd_root%\Runs\Referentie.dms" /Resultaten/StartJaar/PlanRegioResults/Vergelijking/meerkosten/Scenario_2/Aansluitingen
+
+"N:\prog\ObjectVision\%exe%\GeoDmsRun.exe" /L"N:\log\%SUBFILENAME%_Referentie_StartJaar_S3_Hoofdindicatoren.txt" "%pd_root%\Runs\Referentie.dms" /Resultaten/StartJaar/PlanRegioResults/Vergelijking/meerkosten/Scenario_3/Hoofdindicatoren
+"N:\prog\ObjectVision\%exe%\GeoDmsRun.exe" /L"N:\log\%SUBFILENAME%_Referentie_StartJaar_S3_NationaleKosten.txt" "%pd_root%\Runs\Referentie.dms" /Resultaten/StartJaar/PlanRegioResults/Vergelijking/meerkosten/Scenario_3/NationaleKosten
+"N:\prog\ObjectVision\%exe%\GeoDmsRun.exe" /L"N:\log\%SUBFILENAME%_Referentie_StartJaar_S3_Aansluitingen.txt" "%pd_root%\Runs\Referentie.dms" /Resultaten/StartJaar/PlanRegioResults/Vergelijking/meerkosten/Scenario_3/Aansluitingen
+
+"N:\prog\ObjectVision\%exe%\GeoDmsRun.exe" /L"N:\log\%SUBFILENAME%_Referentie_StartJaar_S4_Hoofdindicatoren.txt" "%pd_root%\Runs\Referentie.dms" /Resultaten/StartJaar/PlanRegioResults/Vergelijking/meerkosten/Scenario_4/Hoofdindicatoren
+"N:\prog\ObjectVision\%exe%\GeoDmsRun.exe" /L"N:\log\%SUBFILENAME%_Referentie_StartJaar_S4_NationaleKosten.txt" "%pd_root%\Runs\Referentie.dms" /Resultaten/StartJaar/PlanRegioResults/Vergelijking/meerkosten/Scenario_4/NationaleKosten
+"N:\prog\ObjectVision\%exe%\GeoDmsRun.exe" /L"N:\log\%SUBFILENAME%_Referentie_StartJaar_S4_Aansluitingen.txt" "%pd_root%\Runs\Referentie.dms" /Resultaten/StartJaar/PlanRegioResults/Vergelijking/meerkosten/Scenario_4/Aansluitingen
+
+"N:\prog\ObjectVision\%exe%\GeoDmsRun.exe" /L"N:\log\%SUBFILENAME%_Referentie_StartJaar_S5_Hoofdindicatoren.txt" "%pd_root%\Runs\Referentie.dms" /Resultaten/StartJaar/PlanRegioResults/Vergelijking/meerkosten/Scenario_5/Hoofdindicatoren
+"N:\prog\ObjectVision\%exe%\GeoDmsRun.exe" /L"N:\log\%SUBFILENAME%_Referentie_StartJaar_S5_NationaleKosten.txt" "%pd_root%\Runs\Referentie.dms" /Resultaten/StartJaar/PlanRegioResults/Vergelijking/meerkosten/Scenario_5/NationaleKosten
+"N:\prog\ObjectVision\%exe%\GeoDmsRun.exe" /L"N:\log\%SUBFILENAME%_Referentie_StartJaar_S5_Aansluitingen.txt" "%pd_root%\Runs\Referentie.dms" /Resultaten/StartJaar/PlanRegioResults/Vergelijking/meerkosten/Scenario_5/Aansluitingen
+
+"N:\prog\ObjectVision\%exe%\GeoDmsRun.exe" /L"N:\log\%SUBFILENAME%_Referentie_StartJaar_S6_Hoofdindicatoren.txt" "%pd_root%\Runs\Referentie.dms" /Resultaten/StartJaar/PlanRegioResults/Vergelijking/meerkosten/Scenario_6/Hoofdindicatoren
+"N:\prog\ObjectVision\%exe%\GeoDmsRun.exe" /L"N:\log\%SUBFILENAME%_Referentie_StartJaar_S6_NationaleKosten.txt" "%pd_root%\Runs\Referentie.dms" /Resultaten/StartJaar/PlanRegioResults/Vergelijking/meerkosten/Scenario_6/NationaleKosten
+"N:\prog\ObjectVision\%exe%\GeoDmsRun.exe" /L"N:\log\%SUBFILENAME%_Referentie_StartJaar_S6_Aansluitingen.txt" "%pd_root%\Runs\Referentie.dms" /Resultaten/StartJaar/PlanRegioResults/Vergelijking/meerkosten/Scenario_6/Aansluitingen
+
+PAUSE
