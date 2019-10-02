419f8c53e77dd7c7bbf5a13b9629c6af9fbe4751
basvanbemmel <basvanbemmel@b1a6ab50-0fb6-e611-ad82-1cc1de80c1ee>
2019-10-02 08:47:32 +0000

    statisch in aparte batch onderbrengen
    git-svn-id: https://pbl.sliksvn.com/vesta/PD/branch/Vesta35beta@325 b1a6ab50-0fb6-e611-ad82-1cc1de80c1ee

---
+++ Runs/batch_leidraad_statisch.bat
@@ -0,0 +1,23 @@
+REM GeoDmsRun.exe [/PProjName] [/LLogFileName] ConfigFileName ItemNames
+REM [] betekent optioneel
+REM Sluit GUI-variant(GeoDmsGui.exe) af bij aanroepen van GeoDmsRun.exe (indien dit dezelfde is)
+REM Resultaten worden weggeschreven in: n:/LD/%confignaam%/results/...
+REM Bij nogmaals runnen worden bestanden gewoon overschreven
+
+set pd_root=N:\PD\_Bas\Vesta35beta_Bas
+set exe=GeoDms7182
+
+set CUR_YYYY=%date:~10,4%
+set CUR_MM=%date:~4,2%
+set CUR_DD=%date:~7,2%
+set CUR_HH=%time:~0,2%
+if %CUR_HH% lss 10 (set CUR_HH=0%time:~1,1%)
+set CUR_NN=%time:~3,2%
+set CUR_SS=%time:~6,2%
+set CUR_MS=%time:~9,2%
+set SUBFILENAME=%CUR_YYYY%%CUR_MM%%CUR_DD%-%CUR_HH%%CUR_NN%%CUR_SS%
+
+if not exist %pd_root% goto error
+
+
+"N:\prog\ObjectVision\%exe%\GeoDmsRun.exe" /L"N:\log_bas\%SUBFILENAME%_Statisch.txt" "%pd_root%\Runs\S0_Referentie.dms" /Resultaten/StartJaar/PlanRegioResults/Statisch
--- Runs/batch_leidraad_totaal.bat
+++ Runs/batch_leidraad_totaal.bat
@@ -1,3 +1,4 @@
+call batch_leidraad_statisch.bat
 call batch_leidraad_runs.bat
 call batch_leidraad_runs_NL.bat
 call batch_leidraad_datapakket.bat
