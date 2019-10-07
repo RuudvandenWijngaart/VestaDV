204d2ec876341bfc50debd55921d9e888d829d86
basvanbemmel <basvanbemmel@b1a6ab50-0fb6-e611-ad82-1cc1de80c1ee>
2019-10-07 13:13:08 +0000

    1 verwijzing voor log_root (maar op 1 plek per bestand aan te passen)
    git-svn-id: https://pbl.sliksvn.com/vesta/PD/branch/Vesta35beta@342 b1a6ab50-0fb6-e611-ad82-1cc1de80c1ee

--- Runs/batch_leidraad_datapakket.bat
+++ Runs/batch_leidraad_datapakket.bat
@@ -5,6 +5,7 @@ REM Resultaten worden weggeschreven in: n:/LD/%confignaam%/results/...
 REM Bij nogmaals runnen worden bestanden gewoon overschreven
 
 set pd_root=N:\PD\_Bas\Vesta35beta_Bas
+set log_root=N:\log_bas
 set exe=GeoDms7182
 
 set CUR_YYYY=%date:~10,4%
@@ -21,37 +22,37 @@ if not exist %pd_root% goto error
 
 
 
-"N:\prog\ObjectVision\%exe%\GeoDmsRun.exe" /L"N:\log_bas\%SUBFILENAME%_S0_Referentie_StartJaar_DataPakketExport_NL.txt" "%pd_root%\Runs\S0_Referentie.dms" /Resultaten/StartJaar/DataPakketExport/bestaand/export_csv_nl/result
-"N:\prog\ObjectVision\%exe%\GeoDmsRun.exe" /L"N:\log_bas\%SUBFILENAME%_S0_Referentie_R2_2030_DataPakketExport_NL.txt" "%pd_root%\Runs\S0_Referentie.dms" /Resultaten/R2_2030/DataPakketExport/bestaand/export_csv_nl/result
-"N:\prog\ObjectVision\%exe%\GeoDmsRun.exe" /L"N:\log_bas\%SUBFILENAME%_S1a_AllElectric_lucht_R2_2030_DataPakketExport_NL.txt" "%pd_root%\Runs\S1a_AllElectric_lucht.dms" /Resultaten/R2_2030/DataPakketExport/bestaand/export_csv_nl/result
-"N:\prog\ObjectVision\%exe%\GeoDmsRun.exe" /L"N:\log_bas\%SUBFILENAME%_S1b_AllElectric_bodem_R2_2030_DataPakketExport_NL.txt" "%pd_root%\Runs\S1b_AllElectric_bodem.dms" /Resultaten/R2_2030/DataPakketExport/bestaand/export_csv_nl/result
-"N:\prog\ObjectVision\%exe%\GeoDmsRun.exe" /L"N:\log_bas\%SUBFILENAME%_S2a_restwarmte_R1_2030_DataPakketExport_NL.txt" "%pd_root%\Runs\S2a_restwarmte.dms" /Resultaten/R1_2030/DataPakketExport/bestaand/export_csv_nl/result
-"N:\prog\ObjectVision\%exe%\GeoDmsRun.exe" /L"N:\log_bas\%SUBFILENAME%_S2b_Geothermie-metcontour_R1_2030_DataPakketExport_NL.txt" "%pd_root%\Runs\S2b_Geothermie-metcontour.dms" /Resultaten/R1_2030/DataPakketExport/bestaand/export_csv_nl/result
-"N:\prog\ObjectVision\%exe%\GeoDmsRun.exe" /L"N:\log_bas\%SUBFILENAME%_S2c_Geothermie-zondercontour_R1_2030_DataPakketExport_NL.txt" "%pd_root%\Runs\S2c_Geothermie-zondercontour.dms" /Resultaten/R1_2030/DataPakketExport/bestaand/export_csv_nl/result
-"N:\prog\ObjectVision\%exe%\GeoDmsRun.exe" /L"N:\log_bas\%SUBFILENAME%_S2d_BioWKK_R1_2030_DataPakketExport_NL.txt" "%pd_root%\Runs\S2d_BioWKK.dms" /Resultaten/R1_2030/DataPakketExport/bestaand/export_csv_nl/result
-"N:\prog\ObjectVision\%exe%\GeoDmsRun.exe" /L"N:\log_bas\%SUBFILENAME%_S3a_LT3030_R2_2030_DataPakketExport_NL.txt" "%pd_root%\Runs\S3a_LT3030.dms" /Resultaten/R2_2030/DataPakketExport/bestaand/export_csv_nl/result
-"N:\prog\ObjectVision\%exe%\GeoDmsRun.exe" /L"N:\log_bas\%SUBFILENAME%_S3b_LT3070_R2_2030_DataPakketExport_NL.txt" "%pd_root%\Runs\S3b_LT3070.dms" /Resultaten/R2_2030/DataPakketExport/bestaand/export_csv_nl/result
-"N:\prog\ObjectVision\%exe%\GeoDmsRun.exe" /L"N:\log_bas\%SUBFILENAME%_S3c_LT3050_R2_2030_DataPakketExport_NL.txt" "%pd_root%\Runs\S3c_LT3050.dms" /Resultaten/R2_2030/DataPakketExport/bestaand/export_csv_nl/result
-"N:\prog\ObjectVision\%exe%\GeoDmsRun.exe" /L"N:\log_bas\%SUBFILENAME%_S3d_WKO_R2_2030_DataPakketExport_NL.txt" "%pd_root%\Runs\S3d_WKO.dms" /Resultaten/R2_2030/DataPakketExport/bestaand/export_csv_nl/result
-"N:\prog\ObjectVision\%exe%\GeoDmsRun.exe" /L"N:\log_bas\%SUBFILENAME%_S3e_TEO_R2_2030_DataPakketExport_NL.txt" "%pd_root%\Runs\S3e_TEO.dms" /Resultaten/R2_2030/DataPakketExport/bestaand/export_csv_nl/result
-"N:\prog\ObjectVision\%exe%\GeoDmsRun.exe" /L"N:\log_bas\%SUBFILENAME%_S4_hwp_GG_R1_2030_DataPakketExport_NL.txt" "%pd_root%\Runs\S4_hwp_GG.dms" /Resultaten/R1_2030/DataPakketExport/bestaand/export_csv_nl/result
-"N:\prog\ObjectVision\%exe%\GeoDmsRun.exe" /L"N:\log_bas\%SUBFILENAME%_S5_HR_GG_R1_2030_DataPakketExport_NL.txt" "%pd_root%\Runs\S5_HR_GG.dms" /Resultaten/R1_2030/DataPakketExport/bestaand/export_csv_nl/result
+"N:\prog\ObjectVision\%exe%\GeoDmsRun.exe" /L"%log_root%\%SUBFILENAME%_S0_Referentie_StartJaar_DataPakketExport_NL.txt" "%pd_root%\Runs\S0_Referentie.dms" /Resultaten/StartJaar/DataPakketExport/bestaand/export_csv_nl/result
+"N:\prog\ObjectVision\%exe%\GeoDmsRun.exe" /L"%log_root%\%SUBFILENAME%_S0_Referentie_R2_2030_DataPakketExport_NL.txt" "%pd_root%\Runs\S0_Referentie.dms" /Resultaten/R2_2030/DataPakketExport/bestaand/export_csv_nl/result
+"N:\prog\ObjectVision\%exe%\GeoDmsRun.exe" /L"%log_root%\%SUBFILENAME%_S1a_AllElectric_lucht_R2_2030_DataPakketExport_NL.txt" "%pd_root%\Runs\S1a_AllElectric_lucht.dms" /Resultaten/R2_2030/DataPakketExport/bestaand/export_csv_nl/result
+"N:\prog\ObjectVision\%exe%\GeoDmsRun.exe" /L"%log_root%\%SUBFILENAME%_S1b_AllElectric_bodem_R2_2030_DataPakketExport_NL.txt" "%pd_root%\Runs\S1b_AllElectric_bodem.dms" /Resultaten/R2_2030/DataPakketExport/bestaand/export_csv_nl/result
+"N:\prog\ObjectVision\%exe%\GeoDmsRun.exe" /L"%log_root%\%SUBFILENAME%_S2a_restwarmte_R1_2030_DataPakketExport_NL.txt" "%pd_root%\Runs\S2a_restwarmte.dms" /Resultaten/R1_2030/DataPakketExport/bestaand/export_csv_nl/result
+"N:\prog\ObjectVision\%exe%\GeoDmsRun.exe" /L"%log_root%\%SUBFILENAME%_S2b_Geothermie-metcontour_R1_2030_DataPakketExport_NL.txt" "%pd_root%\Runs\S2b_Geothermie-metcontour.dms" /Resultaten/R1_2030/DataPakketExport/bestaand/export_csv_nl/result
+"N:\prog\ObjectVision\%exe%\GeoDmsRun.exe" /L"%log_root%\%SUBFILENAME%_S2c_Geothermie-zondercontour_R1_2030_DataPakketExport_NL.txt" "%pd_root%\Runs\S2c_Geothermie-zondercontour.dms" /Resultaten/R1_2030/DataPakketExport/bestaand/export_csv_nl/result
+"N:\prog\ObjectVision\%exe%\GeoDmsRun.exe" /L"%log_root%\%SUBFILENAME%_S2d_BioWKK_R1_2030_DataPakketExport_NL.txt" "%pd_root%\Runs\S2d_BioWKK.dms" /Resultaten/R1_2030/DataPakketExport/bestaand/export_csv_nl/result
+"N:\prog\ObjectVision\%exe%\GeoDmsRun.exe" /L"%log_root%\%SUBFILENAME%_S3a_LT3030_R2_2030_DataPakketExport_NL.txt" "%pd_root%\Runs\S3a_LT3030.dms" /Resultaten/R2_2030/DataPakketExport/bestaand/export_csv_nl/result
+"N:\prog\ObjectVision\%exe%\GeoDmsRun.exe" /L"%log_root%\%SUBFILENAME%_S3b_LT3070_R2_2030_DataPakketExport_NL.txt" "%pd_root%\Runs\S3b_LT3070.dms" /Resultaten/R2_2030/DataPakketExport/bestaand/export_csv_nl/result
+"N:\prog\ObjectVision\%exe%\GeoDmsRun.exe" /L"%log_root%\%SUBFILENAME%_S3c_LT3050_R2_2030_DataPakketExport_NL.txt" "%pd_root%\Runs\S3c_LT3050.dms" /Resultaten/R2_2030/DataPakketExport/bestaand/export_csv_nl/result
+"N:\prog\ObjectVision\%exe%\GeoDmsRun.exe" /L"%log_root%\%SUBFILENAME%_S3d_WKO_R2_2030_DataPakketExport_NL.txt" "%pd_root%\Runs\S3d_WKO.dms" /Resultaten/R2_2030/DataPakketExport/bestaand/export_csv_nl/result
+"N:\prog\ObjectVision\%exe%\GeoDmsRun.exe" /L"%log_root%\%SUBFILENAME%_S3e_TEO_R2_2030_DataPakketExport_NL.txt" "%pd_root%\Runs\S3e_TEO.dms" /Resultaten/R2_2030/DataPakketExport/bestaand/export_csv_nl/result
+"N:\prog\ObjectVision\%exe%\GeoDmsRun.exe" /L"%log_root%\%SUBFILENAME%_S4_hwp_GG_R1_2030_DataPakketExport_NL.txt" "%pd_root%\Runs\S4_hwp_GG.dms" /Resultaten/R1_2030/DataPakketExport/bestaand/export_csv_nl/result
+"N:\prog\ObjectVision\%exe%\GeoDmsRun.exe" /L"%log_root%\%SUBFILENAME%_S5_HR_GG_R1_2030_DataPakketExport_NL.txt" "%pd_root%\Runs\S5_HR_GG.dms" /Resultaten/R1_2030/DataPakketExport/bestaand/export_csv_nl/result
 
 
-"N:\prog\ObjectVision\%exe%\GeoDmsRun.exe" /L"N:\log_bas\%SUBFILENAME%_S0_Referentie_StartJaar_DataPakketExport.txt" "%pd_root%\Runs\S0_Referentie.dms" /Resultaten/StartJaar/DataPakketExport/bestaand/generate_all
-"N:\prog\ObjectVision\%exe%\GeoDmsRun.exe" /L"N:\log_bas\%SUBFILENAME%_S0_Referentie_R2_2030_DataPakketExport.txt" "%pd_root%\Runs\S0_Referentie.dms" /Resultaten/R2_2030/DataPakketExport/bestaand/generate_all
-"N:\prog\ObjectVision\%exe%\GeoDmsRun.exe" /L"N:\log_bas\%SUBFILENAME%_S1a_AllElectric_lucht_R2_2030_DataPakketExport.txt" "%pd_root%\Runs\S1a_AllElectric_lucht.dms" /Resultaten/R2_2030/DataPakketExport/bestaand/generate_all
-"N:\prog\ObjectVision\%exe%\GeoDmsRun.exe" /L"N:\log_bas\%SUBFILENAME%_S1b_AllElectric_bodem_R2_2030_DataPakketExport.txt" "%pd_root%\Runs\S1b_AllElectric_bodem.dms" /Resultaten/R2_2030/DataPakketExport/bestaand/generate_all
-"N:\prog\ObjectVision\%exe%\GeoDmsRun.exe" /L"N:\log_bas\%SUBFILENAME%_S2a_restwarmte_R1_2030_DataPakketExport.txt" "%pd_root%\Runs\S2a_restwarmte.dms" /Resultaten/R1_2030/DataPakketExport/bestaand/generate_all
-"N:\prog\ObjectVision\%exe%\GeoDmsRun.exe" /L"N:\log_bas\%SUBFILENAME%_S2b_Geothermie-metcontour_R1_2030_DataPakketExport.txt" "%pd_root%\Runs\S2b_Geothermie-metcontour.dms" /Resultaten/R1_2030/DataPakketExport/bestaand/generate_all
-"N:\prog\ObjectVision\%exe%\GeoDmsRun.exe" /L"N:\log_bas\%SUBFILENAME%_S2c_Geothermie-zondercontour_R1_2030_DataPakketExport.txt" "%pd_root%\Runs\S2c_Geothermie-zondercontour.dms" /Resultaten/R1_2030/DataPakketExport/bestaand/generate_all
-"N:\prog\ObjectVision\%exe%\GeoDmsRun.exe" /L"N:\log_bas\%SUBFILENAME%_S2d_BioWKK_R1_2030_DataPakketExport.txt" "%pd_root%\Runs\S2d_BioWKK.dms" /Resultaten/R1_2030/DataPakketExport/bestaand/generate_all
-"N:\prog\ObjectVision\%exe%\GeoDmsRun.exe" /L"N:\log_bas\%SUBFILENAME%_S3a_LT3030_R2_2030_DataPakketExport.txt" "%pd_root%\Runs\S3a_LT3030.dms" /Resultaten/R2_2030/DataPakketExport/bestaand/generate_all
-"N:\prog\ObjectVision\%exe%\GeoDmsRun.exe" /L"N:\log_bas\%SUBFILENAME%_S3b_LT3070_R2_2030_DataPakketExport.txt" "%pd_root%\Runs\S3b_LT3070.dms" /Resultaten/R2_2030/DataPakketExport/bestaand/generate_all
-"N:\prog\ObjectVision\%exe%\GeoDmsRun.exe" /L"N:\log_bas\%SUBFILENAME%_S3c_LT3050_R2_2030_DataPakketExport.txt" "%pd_root%\Runs\S3c_LT3050.dms" /Resultaten/R2_2030/DataPakketExport/bestaand/generate_all
-"N:\prog\ObjectVision\%exe%\GeoDmsRun.exe" /L"N:\log_bas\%SUBFILENAME%_S3d_WKO_R2_2030_DataPakketExport.txt" "%pd_root%\Runs\S3d_WKO.dms" /Resultaten/R2_2030/DataPakketExport/bestaand/generate_all
-"N:\prog\ObjectVision\%exe%\GeoDmsRun.exe" /L"N:\log_bas\%SUBFILENAME%_S3e_TEO_R2_2030_DataPakketExport.txt" "%pd_root%\Runs\S3e_TEO.dms" /Resultaten/R2_2030/DataPakketExport/bestaand/generate_all
-"N:\prog\ObjectVision\%exe%\GeoDmsRun.exe" /L"N:\log_bas\%SUBFILENAME%_S4_hwp_GG_R1_2030_DataPakketExport.txt" "%pd_root%\Runs\S4_hwp_GG.dms" /Resultaten/R1_2030/DataPakketExport/bestaand/generate_all
-"N:\prog\ObjectVision\%exe%\GeoDmsRun.exe" /L"N:\log_bas\%SUBFILENAME%_S5_HR_GG_R1_2030_DataPakketExport.txt" "%pd_root%\Runs\S5_HR_GG.dms" /Resultaten/R1_2030/DataPakketExport/bestaand/generate_all
+"N:\prog\ObjectVision\%exe%\GeoDmsRun.exe" /L"%log_root%\%SUBFILENAME%_S0_Referentie_StartJaar_DataPakketExport.txt" "%pd_root%\Runs\S0_Referentie.dms" /Resultaten/StartJaar/DataPakketExport/bestaand/generate_all
+"N:\prog\ObjectVision\%exe%\GeoDmsRun.exe" /L"%log_root%\%SUBFILENAME%_S0_Referentie_R2_2030_DataPakketExport.txt" "%pd_root%\Runs\S0_Referentie.dms" /Resultaten/R2_2030/DataPakketExport/bestaand/generate_all
+"N:\prog\ObjectVision\%exe%\GeoDmsRun.exe" /L"%log_root%\%SUBFILENAME%_S1a_AllElectric_lucht_R2_2030_DataPakketExport.txt" "%pd_root%\Runs\S1a_AllElectric_lucht.dms" /Resultaten/R2_2030/DataPakketExport/bestaand/generate_all
+"N:\prog\ObjectVision\%exe%\GeoDmsRun.exe" /L"%log_root%\%SUBFILENAME%_S1b_AllElectric_bodem_R2_2030_DataPakketExport.txt" "%pd_root%\Runs\S1b_AllElectric_bodem.dms" /Resultaten/R2_2030/DataPakketExport/bestaand/generate_all
+"N:\prog\ObjectVision\%exe%\GeoDmsRun.exe" /L"%log_root%\%SUBFILENAME%_S2a_restwarmte_R1_2030_DataPakketExport.txt" "%pd_root%\Runs\S2a_restwarmte.dms" /Resultaten/R1_2030/DataPakketExport/bestaand/generate_all
+"N:\prog\ObjectVision\%exe%\GeoDmsRun.exe" /L"%log_root%\%SUBFILENAME%_S2b_Geothermie-metcontour_R1_2030_DataPakketExport.txt" "%pd_root%\Runs\S2b_Geothermie-metcontour.dms" /Resultaten/R1_2030/DataPakketExport/bestaand/generate_all
+"N:\prog\ObjectVision\%exe%\GeoDmsRun.exe" /L"%log_root%\%SUBFILENAME%_S2c_Geothermie-zondercontour_R1_2030_DataPakketExport.txt" "%pd_root%\Runs\S2c_Geothermie-zondercontour.dms" /Resultaten/R1_2030/DataPakketExport/bestaand/generate_all
+"N:\prog\ObjectVision\%exe%\GeoDmsRun.exe" /L"%log_root%\%SUBFILENAME%_S2d_BioWKK_R1_2030_DataPakketExport.txt" "%pd_root%\Runs\S2d_BioWKK.dms" /Resultaten/R1_2030/DataPakketExport/bestaand/generate_all
+"N:\prog\ObjectVision\%exe%\GeoDmsRun.exe" /L"%log_root%\%SUBFILENAME%_S3a_LT3030_R2_2030_DataPakketExport.txt" "%pd_root%\Runs\S3a_LT3030.dms" /Resultaten/R2_2030/DataPakketExport/bestaand/generate_all
+"N:\prog\ObjectVision\%exe%\GeoDmsRun.exe" /L"%log_root%\%SUBFILENAME%_S3b_LT3070_R2_2030_DataPakketExport.txt" "%pd_root%\Runs\S3b_LT3070.dms" /Resultaten/R2_2030/DataPakketExport/bestaand/generate_all
+"N:\prog\ObjectVision\%exe%\GeoDmsRun.exe" /L"%log_root%\%SUBFILENAME%_S3c_LT3050_R2_2030_DataPakketExport.txt" "%pd_root%\Runs\S3c_LT3050.dms" /Resultaten/R2_2030/DataPakketExport/bestaand/generate_all
+"N:\prog\ObjectVision\%exe%\GeoDmsRun.exe" /L"%log_root%\%SUBFILENAME%_S3d_WKO_R2_2030_DataPakketExport.txt" "%pd_root%\Runs\S3d_WKO.dms" /Resultaten/R2_2030/DataPakketExport/bestaand/generate_all
+"N:\prog\ObjectVision\%exe%\GeoDmsRun.exe" /L"%log_root%\%SUBFILENAME%_S3e_TEO_R2_2030_DataPakketExport.txt" "%pd_root%\Runs\S3e_TEO.dms" /Resultaten/R2_2030/DataPakketExport/bestaand/generate_all
+"N:\prog\ObjectVision\%exe%\GeoDmsRun.exe" /L"%log_root%\%SUBFILENAME%_S4_hwp_GG_R1_2030_DataPakketExport.txt" "%pd_root%\Runs\S4_hwp_GG.dms" /Resultaten/R1_2030/DataPakketExport/bestaand/generate_all
+"N:\prog\ObjectVision\%exe%\GeoDmsRun.exe" /L"%log_root%\%SUBFILENAME%_S5_HR_GG_R1_2030_DataPakketExport.txt" "%pd_root%\Runs\S5_HR_GG.dms" /Resultaten/R1_2030/DataPakketExport/bestaand/generate_all
 
 
--- Runs/batch_leidraad_statisch.bat
+++ Runs/batch_leidraad_statisch.bat
@@ -5,6 +5,7 @@ REM Resultaten worden weggeschreven in: n:/LD/%confignaam%/results/...
 REM Bij nogmaals runnen worden bestanden gewoon overschreven
 
 set pd_root=N:\PD\_Bas\Vesta35beta_Bas
+set log_root=N:\log_bas
 set exe=GeoDms7182
 
 set CUR_YYYY=%date:~10,4%
@@ -20,4 +21,4 @@ set SUBFILENAME=%CUR_YYYY%%CUR_MM%%CUR_DD%-%CUR_HH%%CUR_NN%%CUR_SS%
 if not exist %pd_root% goto error
 
 
-"N:\prog\ObjectVision\%exe%\GeoDmsRun.exe" /L"N:\log_bas\%SUBFILENAME%_Statisch.txt" "%pd_root%\Runs\S0_Referentie.dms" /Resultaten/StartJaar/PlanRegioResults/Statisch
+"N:\prog\ObjectVision\%exe%\GeoDmsRun.exe" /L"%log_root%\%SUBFILENAME%_Statisch.txt" "%pd_root%\Runs\S0_Referentie.dms" /Resultaten/StartJaar/PlanRegioResults/Statisch
