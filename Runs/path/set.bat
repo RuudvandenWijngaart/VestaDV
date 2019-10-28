65c777989e9d4a9c4df3722a9beb036f0556b7f9
maartenhilferink <maartenhilferink@b1a6ab50-0fb6-e611-ad82-1cc1de80c1ee>
2019-10-28 13:15:50 +0000

    Pas leidraad exports toe voor StartJaar en laatste rekenstap.
    git-svn-id: https://pbl.sliksvn.com/vesta/PD/branch/Vesta40_beta@407 b1a6ab50-0fb6-e611-ad82-1cc1de80c1ee

--- Runs/path/set.bat
+++ Runs/path/set.bat
@@ -3,9 +3,9 @@ set prj_dir=N:\PD\Runs_Leidraad\Default_Configuratie_Referentie
 set log_dir=N:\log_vesta
 set exe_dir=N:\prog\ObjectVision\GeoDms7182\GeoDmsRun.exe
 
-REM set prj_dir=D:\prj\Vesta35beta
-REM set log_dir=D:\tmp\log_vesta
-REM set exe_dir=C:\Program Files\ObjectVision\GeoDms7182
-REM einde aanpassingen voor machine specifieke paden
+set prj_dir=..
+set log_dir=C:\LocalData\log_vesta
+set exe_dir=C:\Program Files\ObjectVision\GeoDms7202
+einde aanpassingen voor machine specifieke paden
 
 set exe_path=%exe_dir%\GeoDMSRun.exe
