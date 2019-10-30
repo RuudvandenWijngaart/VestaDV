67645c57a304f58b9c8c1605af1fa58f9f01b18f
basvanbemmel <basvanbemmel@b1a6ab50-0fb6-e611-ad82-1cc1de80c1ee>
2019-10-30 16:33:48 +0000

    DataPakketExportsBebouwingsComponent.dms van revision 415 https://pbl.sliksvn.com/vesta/PD/branch/VestaLeidraadBas NLResults.dms output buurt in batch
    git-svn-id: https://pbl.sliksvn.com/vesta/PD/branch/VestaLeidraad@423 b1a6ab50-0fb6-e611-ad82-1cc1de80c1ee

--- Runs/path/set.bat
+++ Runs/path/set.bat
@@ -1,7 +1,7 @@
 REM aanpassingen voor machine specifieke paden
-set prj_dir=N:\PD\Runs_Leidraad\Default_Configuratie_Referentie
+set prj_dir=N:\PD\_Bas\VestaLeidraad_Bas
 set log_dir=N:\log_vesta
-set exe_dir=N:\prog\ObjectVision\GeoDms7182\GeoDmsRun.exe
+set exe_dir=N:\prog\ObjectVision\GeoDms7182
 
 REM set prj_dir=D:\prj\Vesta35beta
 REM set log_dir=D:\tmp\log_vesta
