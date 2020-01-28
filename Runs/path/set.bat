d4ee93289f9edd320a9d2d38b63bdcd2ef494cbb
basvanbemmel <bas.vanbemmel@pbl.nl>
2020-01-28 09:22:08 +0100

    wijziging paden, verwijderen dubbele settings

--- Runs/path/set.bat
+++ Runs/path/set.bat
@@ -1,10 +1,6 @@
 REM aanpassingen voor machine specifieke paden
-set prj_dir=N:\PD\Runs_Leidraad\Default_Configuratie_Referentie
-set log_dir=N:\log_vesta
-set exe_dir=N:\prog\ObjectVision\GeoDms7182\GeoDmsRun.exe
-
-set prj_dir=C:\prj\pbl\Vesta40_beta
-set log_dir=C:\LocalData\log_vesta
+set prj_dir=C:\Vesta\VestaDV-Vesta40
+set log_dir=C:\Vesta\LD\log_vesta
 set exe_dir=C:\Program Files\ObjectVision\GeoDms7202
 
 REM einde aanpassingen voor machine specifieke paden
