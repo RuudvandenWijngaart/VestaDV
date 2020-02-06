45b3083e596489209d41afa91dacb060f09a0e7e
MolenvdF <folckert.vandermolen@pbl.nl>
2020-02-06 17:17:51 +0100

    Folckert: splitsing BodemWP doorgevoerd in s3 varianten

--- Runs/batch_leidraad_runs.bat
+++ Runs/batch_leidraad_runs.bat
@@ -34,26 +34,26 @@ set ITEMS=%ITEM1% %ITEM2% %ITEM3%
 "%exe_path%" /L"%log_dir%\S2b_B_Geo_contour.txt" "%prj_dir%\Runs\S2b_B_Geo_contour.dms" %ITEMS%
 "%exe_path%" /L"%log_dir%\S2c_B_Geo_overal.txt"  "%prj_dir%\Runs\S2c_B_Geo_overal.dms"  %ITEMS%
 "%exe_path%" /L"%log_dir%\S2d_D_Restwarmte.txt"  "%prj_dir%\Runs\S2d_D_Restwarmte.dms"  %ITEMS%
-"%exe_path%" /L"%log_dir%\S2e_D_Geo_contour.txt" "%prj_dir%\Runs\S2e_D_Geo_contour"     %ITEMS%
-"%exe_path%" /L"%log_dir%\S2f_D_Geo_overal.txt"  "%prj_dir%\Runs\S2f_D_Geo_overal"      %ITEMS%
+"%exe_path%" /L"%log_dir%\S2e_D_Geo_contour.txt" "%prj_dir%\Runs\S2e_D_Geo_contour.dms" %ITEMS%
+"%exe_path%" /L"%log_dir%\S2f_D_Geo_overal.txt"  "%prj_dir%\Runs\S2f_D_Geo_overal.dms"  %ITEMS%
 
-"%exe_path%" /L"%log_dir%\S3a_B_LT30_30.txt"     "%prj_dir%\Runs\S3a_B_LT30_30"         %ITEMS%
+"%exe_path%" /L"%log_dir%\S3a_B_LT30_30.txt"     "%prj_dir%\Runs\S3a_B_LT30_30.dms"     %ITEMS%
 "%exe_path%" /L"%log_dir%\S3b_B_LT30_70.txt"     "%prj_dir%\Runs\S3b_B_LT30_70.dms"     %ITEMS%
-"%exe_path%" /L"%log_dir%\S3c_B_BuurtWKO.txt"    "%prj_dir%\Runs\S3c_B_BuurtWKO"        %ITEMS%
-"%exe_path%" /L"%log_dir%\S3d_B_WKO.txt"         "%prj_dir%\Runs\S3d_B_WKO"             %ITEMS%
-"%exe_path%" /L"%log_dir%\S3e_B_TEO.txt"         "%prj_dir%\Runs\S3e_B_TEO"             %ITEMS%
+"%exe_path%" /L"%log_dir%\S3c_B_BuurtWKO.txt"    "%prj_dir%\Runs\S3c_B_BuurtWKO.dms"    %ITEMS%
+"%exe_path%" /L"%log_dir%\S3d_B_WKO.txt"         "%prj_dir%\Runs\S3d_B_WKO.dms"         %ITEMS%
+"%exe_path%" /L"%log_dir%\S3e_B_TEO.txt"         "%prj_dir%\Runs\S3e_B_TEO.dms"         %ITEMS%
 "%exe_path%" /L"%log_dir%\S3f_D_LT30_70.txt"     "%prj_dir%\Runs\S3f_D_LT30_70.dms"     %ITEMS%
-"%exe_path%" /L"%log_dir%\S3g_D_BuurtWKO.txt"    "%prj_dir%\Runs\S3g_D_BuurtWKO"        %ITEMS%
-"%exe_path%" /L"%log_dir%\S3h_D_TEO.txt"         "%prj_dir%\Runs\S3h_D_TEO"             %ITEMS%
+"%exe_path%" /L"%log_dir%\S3g_D_BuurtWKO.txt"    "%prj_dir%\Runs\S3g_D_BuurtWKO.dms"    %ITEMS%
+"%exe_path%" /L"%log_dir%\S3h_D_TEO.txt"         "%prj_dir%\Runs\S3h_D_TEO.dms"         %ITEMS%
 
-"%exe_path%" /L"%log_dir%\S4a_H2_B_hWP.txt"      "%prj_dir%\Runs\S4a_H2_B_hWP"          %ITEMS%
+"%exe_path%" /L"%log_dir%\S4a_H2_B_hWP.txt"      "%prj_dir%\Runs\S4a_H2_B_hWP.dms"      %ITEMS%
 "%exe_path%" /L"%log_dir%\S4b_H2_B_HR.txt"       "%prj_dir%\Runs\S4b_H2_B_HR.dms"       %ITEMS%
-"%exe_path%" /L"%log_dir%\S4c_H2_D_hWP.txt"      "%prj_dir%\Runs\S4c_H2_D_hWP"          %ITEMS%
-"%exe_path%" /L"%log_dir%\S4d_H2_D_HR.txt"       "%prj_dir%\Runs\S4d_H2_D_HR"           %ITEMS%
+"%exe_path%" /L"%log_dir%\S4c_H2_D_hWP.txt"      "%prj_dir%\Runs\S4c_H2_D_hWP.dms"      %ITEMS%
+"%exe_path%" /L"%log_dir%\S4d_H2_D_HR.txt"       "%prj_dir%\Runs\S4d_H2_D_HR.dms"       %ITEMS%
 
-"%exe_path%" /L"%log_dir%\S5a_GG_B_hWP.txt"      "%prj_dir%\Runs\S5a_GG_B_hWP"          %ITEMS%
+"%exe_path%" /L"%log_dir%\S5a_GG_B_hWP.txt"      "%prj_dir%\Runs\S5a_GG_B_hWP.dms"      %ITEMS%
 "%exe_path%" /L"%log_dir%\S5b_GG_B_HR.txt"       "%prj_dir%\Runs\S5b_GG_B_HR.dms"       %ITEMS%
-"%exe_path%" /L"%log_dir%\S5c_GG_D_hWP.txt"      "%prj_dir%\Runs\S5c_GG_D_hWP"          %ITEMS%
-"%exe_path%" /L"%log_dir%\S5d_GG_D_HR.txt"       "%prj_dir%\Runs\S5d_GG_D_HR"           %ITEMS%
+"%exe_path%" /L"%log_dir%\S5c_GG_D_hWP.txt"      "%prj_dir%\Runs\S5c_GG_D_hWP.dms"      %ITEMS%
+"%exe_path%" /L"%log_dir%\S5d_GG_D_HR.txt"       "%prj_dir%\Runs\S5d_GG_D_HR.dms"       %ITEMS%
 
 pause Totaal klaar
