a2625ff7322db71f95cfc9f82abde592bfeb9e08
basvanbemmel <bas.vanbemmel@pbl.nl>
2020-08-27 16:30:25 +0200

    set.bat 7.212 def versie Vesta verwijder versiebeheersbestand svn (gebruiken git) git.txt --> alleen lokaal van belang

--- Runs/path/set.bat
+++ Runs/path/set.bat
@@ -1,7 +1,7 @@
 REM aanpassingen voor machine specifieke paden
 set prj_dir=N:\PD\_Bas\GeneralisatieConversie
 set log_dir=N:\LD\_Bas\GeneralisatieConversie\log
-set exe_dir=N:\prog\ObjectVision\GeoDms7219
+set exe_dir=N:\prog\ObjectVision\GeoDms7212
 
 REM einde aanpassingen voor machine specifieke paden
 
