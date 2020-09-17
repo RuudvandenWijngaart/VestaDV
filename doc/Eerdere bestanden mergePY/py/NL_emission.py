66510067307f203a5fd92b5d52ec3df7a7f55e29
basvanbemmel <bas.vanbemmel@pbl.nl>
2020-09-17 13:47:21 +0200

    merge bestanden mbv pyton niet meer ondersteund

--- doc/Eerdere bestanden mergePY/py/NL_emission.py
+++
@@ -1,9 +0,0 @@
-# Sortlist item has for fields: first field: orderingsnumber, second: filename,
-#      third = True: file must exist, False: file may not be there.
-#      Fourth the number of lines in the output
-sortlist = [\
-[1,"NL\All_NOx\Matrix.csv",True,21],\
-[2,"NL\All_SO2\Matrix.csv",True,21],\
-[3,"NL\All_TS\Matrix.csv",True,21],\
-[4,"NL\All_VOS\Matrix.csv",True,21]\
-]
