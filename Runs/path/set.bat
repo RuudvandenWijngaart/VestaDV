a1dc89397dc18294c20485b43f38a43830ec0194
MolenvdF <folckert.vandermolen@pbl.nl>
2021-02-24 12:55:27 +0100

    Batch verwijst nu naar geoDMS 7.238 ipv 7.233

--- Runs/path/set.bat
+++ Runs/path/set.bat
@@ -2,7 +2,7 @@ REM aanpassingen voor machine specifieke paden
 
 set prj_dir=K:\PD\ConversieOS5
 set log_dir=K:\LD\ConversieOS5\log
-set exe_dir=C:\Program Files\ObjectVision\GeoDms7233
+set exe_dir=C:\Program Files\ObjectVision\GeoDms7238
 
 REM einde aanpassingen voor machine specifieke paden
 
