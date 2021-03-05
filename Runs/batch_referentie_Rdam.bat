32f0c61109fdb68bf9b7a313b3b1af665df61118
Folckert van der Molen <folckert.vandermolen@pbl.nl>
2021-03-05 17:26:26 +0100

    Testinstellingen MRDH

---
+++ Runs/batch_referentie_Rdam.bat
@@ -0,0 +1,12 @@
+REM GeoDmsRun.exe [/PProjName] [/LLogFileName] ConfigFileName ItemNames
+REM [] betekent optioneel
+REM Sluit GUI-variant(GeoDmsGui.exe) af bij aanroepen van GeoDmsRun.exe (indien dit dezelfde is)
+REM Resultaten worden weggeschreven in: n:/LD/%confignaam%/results/...
+REM Bij nogmaals runnen worden bestanden gewoon overschreven
+
+REM aanpassingen voor machine specifieke paden
+Call path/set.bat
+
+"%exe_path%" /L"%log_dir%\Referentie_Rdam.txt"  "%prj_dir%\Runs\Referentie_Rdam.dms" /Resultaten/Exports/GenerateAllIndicatoren
+
+pause Totaal klaar
--- Runs/path/set.bat
+++ Runs/path/set.bat
@@ -1,8 +1,8 @@
 REM aanpassingen voor machine specifieke paden
 
-set prj_dir=K:\PD\ConversieOS5
-set log_dir=K:\LD\ConversieOS5\log
-set exe_dir=C:\Program Files\ObjectVision\GeoDms7238
+set prj_dir=G:\PD\VestaDV
+set log_dir=G:\LD\VestaDV\log
+set exe_dir=C:\Program Files\ObjectVision\GeoDms7206
 
 REM einde aanpassingen voor machine specifieke paden
 
