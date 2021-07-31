4b051abb471e228cefb530303e2b122743b5fcee
MolenvdF <folckert.vandermolen@pbl.nl>
2021-07-31 18:41:30 +0200

    GebouwOpties toegepast per segment, verwerking resultaten work in progress

--- Runs/path/set.bat
+++ Runs/path/set.bat
@@ -1,8 +1,8 @@
 REM aanpassingen voor machine specifieke paden
 
-set prj_dir=K:\PD\ConversieOS5
-set log_dir=K:\LD\ConversieOS5\log
-set exe_dir=C:\Program Files\ObjectVision\GeoDms7238
+set prj_dir=C:\Vesta\PD\VestaDV_SAWEC-WP5
+set log_dir=C:\Vesta\LD\VestaDV_SAWEC-WP5\log
+set exe_dir=C:\Program Files\ObjectVision\GeoDms7206
 
 REM einde aanpassingen voor machine specifieke paden
 
