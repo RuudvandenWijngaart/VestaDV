0224ac74098637e79ca698c768104591c1bbdf83
Maarten Hilferink <mhilferink@objectvision.nl>
2021-10-14 18:31:53 +0200

    Vervanging van diverse combine(A, B) door combine_unit(A, B). Laatstgenoemde functies geven geen nr_1 en nr_2 attributen met #A x #B elementen hetgeen tot een data explosie kan leiden igv #A en #B groot en enkel nodig om unique values van combine_data resultaten te bepalen, zoals o.a. bij PlanRegio_x_AfnameGebied, en wellicht InstallatieGetallen.

--- Runs/batch_referentiePerJaar.bat
+++ Runs/batch_referentiePerJaar.bat
@@ -7,6 +7,6 @@ REM Bij nogmaals runnen worden bestanden gewoon overschreven
 REM aanpassingen voor machine specifieke paden
 Call path/set.bat
 
-"%exe_path%" /L"%log_dir%\ReferentiePerJaar.txt"  "%prj_dir%\Runs\ReferentiePerJaar.dms" /Resultaten/Exports/GenerateAllIndicatoren
+START "HESTIA" /WAIT /BELOWNORMAL /NODE 0 /AFFINITY 0x5555 "%exe_path%" "/L%log_dir%\ReferentiePerJaar.txt"  "%prj_dir%\Runs\ReferentiePerJaar.dms" /Resultaten/Exports/GenerateAllIndicatoren
 
 pause Totaal klaar
