8b25f25260ee1cf64066c9391a30290fceaa471e
WesselPoorthuis <wessel.poorthuis@pbl.nl>
2022-06-27 12:50:18 +0200

    Indirect ipv switch voor subjectiev_evaluation

--- Runs/path/set.bat
+++ Runs/path/set.bat
@@ -1,8 +1,8 @@
 REM aanpassingen voor machine specifieke paden
 
-set prj_dir=K:\Hestia\PD
-set log_dir=K:\Hestia\LD\log
-set exe_dir=C:\Program Files\ObjectVision\GeoDms7407
+set prj_dir=C:\Users\wesse\Documents\aaa Uni\Stage\GeoDMS\Hestia\PD
+set log_dir=C:\Users\wesse\Documents\aaa Uni\Stage\GeoDMS\Hestia\LD\log
+set exe_dir=C:\Program Files\ObjectVision\GeoDms7412
 
 REM einde aanpassingen voor machine specifieke paden
 
