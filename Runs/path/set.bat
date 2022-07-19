3e2b59b0b520f7355093d6e735cddddfebb19848
MolenvdF <folckert.vandermolen@pbl.nl>
2022-07-19 21:55:19 +0200

    leercurves opgenomen

--- Runs/path/set.bat
+++ Runs/path/set.bat
@@ -2,8 +2,10 @@ REM aanpassingen voor machine specifieke paden
 
 set prj_dir=C:\Vesta\Model\PD\Hestia
 set log_dir=C:\Vesta\Model\LD\Hestia\log
-set exe_dir=C:\Program Files\ObjectVision\GeoDms7321
+set exe_dir=C:\Program Files\ObjectVision\GeoDms7404
 
 REM einde aanpassingen voor machine specifieke paden
 
 set exe_path=%exe_dir%\GeoDMSRun.exe
+
+
