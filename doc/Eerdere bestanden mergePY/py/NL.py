bab65c5e559828e6853afc08490abe248474fd81
MolenvdF <folckert.vandermolen@pbl.nl>
2022-09-07 12:51:47 +0200

    oud doc removed

--- doc/Eerdere bestanden mergePY/py/NL.py
+++
@@ -1,8 +0,0 @@
-# Sortlist item has for fields: first field: orderingsnumber, second: filename,
-#      third = True: file must exist, False: file may not be there.
-#      Fourth the number of lines in the output
-sortlist = [\
-[1,"NL\All_CO2\Matrix.csv",True,21],\
-[2,"NL\All_Flow\Matrix.csv",True,45],\
-[3,"NL\All_Energy\Matrix.csv",True,21]\
-]
--- doc/Eerdere bestanden mergePY/py/matrix_allflow.py
+++
@@ -1,6 +0,0 @@
-# Sortlist item has for fields: first field: orderingsnumber, second: filename,
-#      third = True: file must exist, False: file may not be there.
-#      Fourth the number of lines in the output
-sortlist = [\
-[1,"NL\All_Flow\Matrix.csv",True,45]\
-]
