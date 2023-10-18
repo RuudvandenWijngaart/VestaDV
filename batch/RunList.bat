97a9a0af04f3330a133e999b8c915e113961bbad
Maarten Hilferink <mhilferink@objectvision.nl>
2023-10-18 13:08:12 +0200

    Run /Resultaten/Exports/GenerateAllIndicatoren, keep some metadata during the whole scheduling phase

--- batch/RunList.bat
+++ batch/RunList.bat
@@ -1,5 +1,6 @@
 REM @USERINPUT: List of runs to process.
-call Impl\RunOneInstance.bat HestiaRun /TussenResultaten/R1_2021/BebouwingsComponenten/Woning/BebouwingsObjectMetGebouwOptie/Metervraag
+REM call Impl\RunOneInstance.bat HestiaRun /TussenResultaten/R1_2021/BebouwingsComponenten/Woning/BebouwingsObjectMetGebouwOptie/Metervraag
+call Impl\RunOneInstance.bat HestiaRun /Resultaten/Exports/GenerateAllIndicatoren
 REM call Impl\RunOneInstance.bat Referentie      /Resultaten/Exports/GenerateAllIndicatoren
 REM call Impl\RunOneInstance.bat Referentie      /Resultaten/Exports/GenerateAllAlloc
 REM call Impl\RunOneInstance.bat Verhoging50cent /Resultaten/Exports/GenerateAllIndicatoren
