63b7bb3d0a2976325d996bbebf8c40e5fc4b1227
Maarten Hilferink <mhilferink@objectvision.nl>
2024-09-06 10:58:13 +0200

    Runs\path\set.bat now refers to GeoDms version 15.7.1

--- Runs/path/set.bat
+++ Runs/path/set.bat
@@ -2,7 +2,7 @@ REM aanpassingen voor machine specifieke paden
 
 set prj_dir=C:\Vesta\PD_SA
 set log_dir=C:\Vesta\LD\PD_SA\log
-set exe_dir=C:\Program Files\ObjectVision\GeoDms14.14.1
+set exe_dir=C:\Program Files\ObjectVision\GeoDms15.7.1
 
 REM einde aanpassingen voor machine specifieke paden
 
