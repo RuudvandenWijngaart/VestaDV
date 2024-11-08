8f4ecec224879c4d4613303833e02c00cdd09ae8
Maarten Hilferink <mhilferink@objectvision.nl>
2024-11-08 21:55:30 +0100

    Lijnbronnen als Restwarmte bronnen

--- Runs/path/set.bat
+++ Runs/path/set.bat
@@ -2,7 +2,7 @@ REM aanpassingen voor machine specifieke paden
 
 set prj_dir=C:\Vesta\PD_SA
 set log_dir=C:\Vesta\LD\PD_SA\log
-set exe_dir=C:\Program Files\ObjectVision\GeoDms15.6.0
+set exe_dir=C:\Program Files\ObjectVision\GeoDms16.0.1
 
 REM einde aanpassingen voor machine specifieke paden
 
