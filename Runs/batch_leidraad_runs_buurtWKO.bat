a16e75fbb021cab673ad309bd556ddcdf93f3197
WesselPoorthuis <wessel.poorthuis@pbl.nl>
2025-06-20 12:17:40 +0200

    Verwijdering buurtTEO S3e

--- Runs/batch_leidraad_runs_buurtWKO.bat
+++ Runs/batch_leidraad_runs_buurtWKO.bat
@@ -11,7 +11,6 @@ set FLAGS=/S1 /C2 /S3
 call :do_variant supplementair_S3b_B_buurtWKO15_15
 call :do_variant supplementair_S3c_B_buurtWKO15_50
 call :do_variant supplementair_S3d_B_buurtWKO15_70
-call :do_variant supplementair_S3e_B_buurtTEO_15_15
 call :do_variant supplementair_S3g_D_buurtWKO15_15
 call :do_variant supplementair_S3h_D_buurtWKO15_70
 
